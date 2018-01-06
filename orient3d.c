ea68070c72f9b219ae5d723a17e4e45183085ef3
micuat <micuat@gmail.com>
2018-01-06 22:02:52 +0000

    mpr121 touch

--- orient3d.c
+++ orient3d.c
@@ -16,7 +16,7 @@
 
 #include "tinyosc.h"
 
-#define I2C_ADDR 0x28
+#define BNO_I2C_ADDR 0x28
 
 #define BNO055_ID 0xA0
 
@@ -41,10 +41,39 @@
 #define POWER_MODE_LOWPOWER 0x01
 #define POWER_MODE_SUSPEND 0x02
 
+#define MPR_I2C_ADDR 0x5A
+
+#define MPR121_ID 0x00
+
+#define MPR121_CHIP_ID_ADDR 0x00
+
+#define MPR121_I2CADDR_DEFAULT 0x5A
+#define MPR121_TOUCHSTATUS_L   0x00
+#define MPR121_FILTDATA_0L     0x04
+#define MPR121_BASELINE_0      0x1E
+#define MPR121_MHDR            0x2B
+#define MPR121_NHDR            0x2C
+#define MPR121_NCLR            0x2D
+#define MPR121_FDLR            0x2E
+#define MPR121_MHDF            0x2F
+#define MPR121_NHDF            0x30
+#define MPR121_NCLF            0x31
+#define MPR121_FDLF            0x32
+#define MPR121_NHDT            0x33
+#define MPR121_NCLT            0x34
+#define MPR121_FDLT            0x35
+#define MPR121_TOUCHTH_0       0x41
+#define MPR121_RELEASETH_0     0x42
+#define MPR121_DEBOUNCE        0x5B
+#define MPR121_CONFIG1         0x5C
+#define MPR121_CONFIG2         0x5D
+#define MPR121_ECR             0x5E
+#define MPR121_SOFTRESET       0x80
 
 int main(int argc, char *argv[])
 {
-   int dev;
+   int devBno;
+   int devMpr;
    if (wiringPiSetup() == -1) {
        printf("wiringPi init failed\n");
        return -1;
@@ -52,42 +81,76 @@ int main(int argc, char *argv[])
 
    pullUpDnControl(8, PUD_UP);
    pullUpDnControl(9, PUD_UP);
-   if ( (dev = wiringPiI2CSetup(I2C_ADDR)) == -1) {
-       printf("error init i2c at 0x%04x\n",I2C_ADDR);
+   if ( (devBno = wiringPiI2CSetup(BNO_I2C_ADDR)) == -1) {
+       printf("error init i2c at 0x%04x\n",BNO_I2C_ADDR);
        return -1;
    }
    else
-       printf("i2c init at 0x%04x OK, dev = %i\n", I2C_ADDR, dev);
+       printf("i2c init at 0x%04x OK, devBno = %i\n", BNO_I2C_ADDR, devBno);
 
 
    // sanity check
-   char id = (char) wiringPiI2CReadReg8(dev, BNO055_CHIP_ID_ADDR);
+   char id = (char) wiringPiI2CReadReg8(devBno, BNO055_CHIP_ID_ADDR);
    printf("chip id: 0x%02X (expected: 0x%02X)\n", id, BNO055_ID);
 
    // set config mode
-   wiringPiI2CWriteReg8(dev, BNO055_OPR_MODE_ADDR, OPERATION_MODE_CONFIG);
+   wiringPiI2CWriteReg8(devBno, BNO055_OPR_MODE_ADDR, OPERATION_MODE_CONFIG);
    usleep(30 * 1000);
 
    // reset
-   wiringPiI2CWriteReg8(dev, BNO055_SYS_TRIGGER_ADDR, 0x20);
-   while (wiringPiI2CReadReg8(dev, BNO055_CHIP_ID_ADDR) != BNO055_ID)
+   wiringPiI2CWriteReg8(devBno, BNO055_SYS_TRIGGER_ADDR, 0x20);
+   while (wiringPiI2CReadReg8(devBno, BNO055_CHIP_ID_ADDR) != BNO055_ID)
    {
       usleep(10 * 1000);
    }
    usleep(50 * 1000);
 
    // normal power mode
-   wiringPiI2CWriteReg8(dev, BNO055_PWR_MODE_ADDR, POWER_MODE_NORMAL);
+   wiringPiI2CWriteReg8(devBno, BNO055_PWR_MODE_ADDR, POWER_MODE_NORMAL);
    usleep(10 * 1000);
 
-   wiringPiI2CWriteReg8(dev, BNO055_PAGE_ID_ADDR, 0);
+   wiringPiI2CWriteReg8(devBno, BNO055_PAGE_ID_ADDR, 0);
 
-   wiringPiI2CWriteReg8(dev, BNO055_SYS_TRIGGER_ADDR, 0x00);
+   wiringPiI2CWriteReg8(devBno, BNO055_SYS_TRIGGER_ADDR, 0x00);
    usleep(10 * 1000);
 
-   wiringPiI2CWriteReg8(dev, BNO055_OPR_MODE_ADDR, OPERATION_MODE_NDOF);
+   wiringPiI2CWriteReg8(devBno, BNO055_OPR_MODE_ADDR, OPERATION_MODE_NDOF);
    usleep(30 * 1000);
    usleep(20 * 1000);
+
+   // MPR121 init
+   if ( (devMpr = wiringPiI2CSetup(MPR_I2C_ADDR)) == -1) {
+    printf("error init i2c at 0x%04x\n",MPR_I2C_ADDR);
+      return -1;
+   }
+   else
+      printf("i2c init at 0x%04x OK, devMpr = %i\n", MPR_I2C_ADDR, devMpr);
+   // https://github.com/adafruit/Adafruit_CircuitPython_MPR121/blob/master/adafruit_mpr121.py
+   wiringPiI2CWriteReg8(devMpr, MPR121_SOFTRESET, 0x63);
+   wiringPiI2CWriteReg8(devMpr, MPR121_ECR, 0x00);
+   // skip read CONFIG2
+   int touch = 12, release = 6;
+   for(int i = 0; i < 12; i ++) {
+      wiringPiI2CWriteReg8(devMpr, MPR121_TOUCHTH_0 + 2*i, touch);
+      wiringPiI2CWriteReg8(devMpr, MPR121_RELEASETH_0 + 2*i, release);
+   }
+   wiringPiI2CWriteReg8(devMpr, MPR121_MHDR, 0x01);
+   wiringPiI2CWriteReg8(devMpr, MPR121_NHDR, 0x01);
+   wiringPiI2CWriteReg8(devMpr, MPR121_NCLR, 0x0E);
+   wiringPiI2CWriteReg8(devMpr, MPR121_FDLR, 0x00);
+   wiringPiI2CWriteReg8(devMpr, MPR121_MHDF, 0x01);
+   wiringPiI2CWriteReg8(devMpr, MPR121_NHDF, 0x05);
+   wiringPiI2CWriteReg8(devMpr, MPR121_NCLF, 0x01);
+   wiringPiI2CWriteReg8(devMpr, MPR121_FDLF, 0x00);
+   wiringPiI2CWriteReg8(devMpr, MPR121_NHDT, 0x00);
+   wiringPiI2CWriteReg8(devMpr, MPR121_NCLT, 0x00);
+   wiringPiI2CWriteReg8(devMpr, MPR121_FDLT, 0x00);
+
+   wiringPiI2CWriteReg8(devMpr, MPR121_DEBOUNCE, 0x00);
+   wiringPiI2CWriteReg8(devMpr, MPR121_CONFIG1, 0x10);
+   wiringPiI2CWriteReg8(devMpr, MPR121_CONFIG2, 0x20);
+
+   wiringPiI2CWriteReg8(devMpr, MPR121_ECR, 0x8F);
 
    // osc init
    // open a socket to listen for datagrams (i.e. UDP packets) on port 9000
@@ -117,14 +180,14 @@ int main(int argc, char *argv[])
 
    while (1)
    {
-      int wl = (int)wiringPiI2CReadReg8(dev, BNO055_QUATERNION_DATA_W_LSB_ADDR);
-      int wm = (int)wiringPiI2CReadReg8(dev, BNO055_QUATERNION_DATA_W_MSB_ADDR);
-      int xl = (int)wiringPiI2CReadReg8(dev, BNO055_QUATERNION_DATA_X_LSB_ADDR);
-      int xm = (int)wiringPiI2CReadReg8(dev, BNO055_QUATERNION_DATA_X_MSB_ADDR);
-      int yl = (int)wiringPiI2CReadReg8(dev, BNO055_QUATERNION_DATA_Y_LSB_ADDR);
-      int ym = (int)wiringPiI2CReadReg8(dev, BNO055_QUATERNION_DATA_Y_MSB_ADDR);
-      int zl = (int)wiringPiI2CReadReg8(dev, BNO055_QUATERNION_DATA_Z_LSB_ADDR);
-      int zm = (int)wiringPiI2CReadReg8(dev, BNO055_QUATERNION_DATA_Z_MSB_ADDR);
+      int wl = (int)wiringPiI2CReadReg8(devBno, BNO055_QUATERNION_DATA_W_LSB_ADDR);
+      int wm = (int)wiringPiI2CReadReg8(devBno, BNO055_QUATERNION_DATA_W_MSB_ADDR);
+      int xl = (int)wiringPiI2CReadReg8(devBno, BNO055_QUATERNION_DATA_X_LSB_ADDR);
+      int xm = (int)wiringPiI2CReadReg8(devBno, BNO055_QUATERNION_DATA_X_MSB_ADDR);
+      int yl = (int)wiringPiI2CReadReg8(devBno, BNO055_QUATERNION_DATA_Y_LSB_ADDR);
+      int ym = (int)wiringPiI2CReadReg8(devBno, BNO055_QUATERNION_DATA_Y_MSB_ADDR);
+      int zl = (int)wiringPiI2CReadReg8(devBno, BNO055_QUATERNION_DATA_Z_LSB_ADDR);
+      int zm = (int)wiringPiI2CReadReg8(devBno, BNO055_QUATERNION_DATA_Z_MSB_ADDR);
 
       float w = ((float)(wl | (wm << 8)) - 32768.0f) / 32768.0f;
       float x = ((float)(xl | (xm << 8)) - 32768.0f) / 32768.0f;
@@ -146,7 +209,11 @@ int main(int argc, char *argv[])
       float cosy = 1 - 2 * (y * y + z * z);
       float yaw = atan2f(siny, cosy) / M_PI * 180;
 
-      printf("roll = %7.2f, pitch = %7.2f, yaw = %7.2f\n", roll, pitch, yaw);
+      //printf("roll = %7.2f, pitch = %7.2f, yaw = %7.2f\n", roll, pitch, yaw);
+
+      int touched = (int)wiringPiI2CReadReg8(devMpr, MPR121_TOUCHSTATUS_L);
+      printf("touchedVal: %d\n", touched);
+
 
       int len = 0;
       char v0[50];
